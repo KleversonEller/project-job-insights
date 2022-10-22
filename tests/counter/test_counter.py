c1d2dc12dda311fe15d3bd5ae093225c080d3d62
Kleverson Eller <kleverson.a16@gmail.com>
2022-10-22 08:14:15 -0300

    test: :white_check_mark: Implementando test (test_counter)
    Adicionando test unitario que valida a função (count_ocurrences)

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    count = count_ocurrences("src/jobs.csv", "javascript")
+
+    assert count == 122
