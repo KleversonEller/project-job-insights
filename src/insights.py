71121f10f7a69c4b820961503656069884bbfbbc
Kleverson Eller <kleverson.a16@gmail.com>
2022-10-22 06:58:29 -0300

    refactor: :recycle: Implementando a função (filter_by_job_type)
    Adicionando funcionalidade que filtra os jobs por um type

--- src/insights.py
+++ src/insights.py
@@ -39,7 +39,11 @@ def filter_by_job_type(jobs, job_type):
     list
         List of jobs with provided job_type
     """
-    return []
+
+    filter_jobs = [
+        type_job for type_job in jobs if type_job["job_type"] == job_type
+    ]
+    return filter_jobs
 
 
 def get_unique_industries(path):
