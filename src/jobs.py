ade5d33186f3105a7661b0f7e56be08a674eabc0
Kleverson Eller <kleverson.a16@gmail.com>
2022-10-22 05:46:10 -0300

    refactor: :recycle: Implementando a função (read)
    Adicionando cuncionalidade que le um arquivo CSV passado por parametro e retorna um lista do mesmo

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,7 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+
+    with open(path) as file:
+        jobs_reader = csv.DictReader(file)
+        return list(jobs_reader)
